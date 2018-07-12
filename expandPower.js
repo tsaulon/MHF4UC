c1c7a5a8ea07e348666db6cc9d29b40d24d49a3b
tsaulon <tyrone.saulon@hotmail.com>
2018-07-12 00:15:30 -0400

    Adding bracket insertion when inputting fractions or negative numbers.

--- expandPower.js
+++ expandPower.js
@@ -3,14 +3,26 @@ let args = process.argv.slice(2).join("");
 
 function expandPowers(value){
     
-    //parse string
-    let tmp = value.split("^");
+    try{
 
-    //extract contents
+    // parse string
+    let tmp = value.split("^");
+    if(tmp.length !== 2){
+        throw new Error('Try 2 arguments.');
+    }
+
+    // extract contents
     let base = tmp[0];
     let power = Number(tmp[1]);
 
-    let expansion = `${value} = `;
+    // check if base is a negative or fraction and does not contain brackets
+    if((base.indexOf('-') > -1 || base.indexOf('/') > -1) &&
+                 base.indexOf('(') === -1 && base.indexOf(')') === -1){
+        base = `(${base})`; //add brackets
+    }
+
+    // expand
+    let expansion = `${base}^${power} = `;
     for(let i = 0; i < power; ++i){
 
         expansion += base;
@@ -21,8 +33,13 @@ function expandPowers(value){
     }
 
     return expansion;
+    
+    } catch(e){
+        return e.message;
+    }
 }
 
 console.log(expandPowers(args));
+console.log(Number(1/2));
 
 // TODO: Handle inverse fractions to be expanded
